977293d7ed195fde5ffc1e74279616b1ab9c96b5
elgedr <elgedr@ttu.ee>
2021-09-01 23:29:40 +0300

    commit 2

--- AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -3,7 +3,7 @@ package ee.ttu.algoritmid.fibonacci;
 public class AL01A {
 
     /**
-     * Compute the Fibonacci sequence number.
+     * Compute the Fibonaci sequence number.
      *
      * @param n The number of the sequence to compute.
      * @return The n-th number in Fibonacci series.
