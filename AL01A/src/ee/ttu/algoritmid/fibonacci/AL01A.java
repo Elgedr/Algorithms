03fe579e3efb89a33848dea8e0c3cc2581f7f073
elgedr <elgedr@ttu.ee>
2021-09-02 14:55:47 +0300

    commit 2

--- AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -1,5 +1,7 @@
 package ee.ttu.algoritmid.fibonacci;
 
+import java.math.BigInteger;
+
 public class AL01A {
 
     /**
@@ -9,7 +11,23 @@ public class AL01A {
      * @return The n-th number in Fibonacci series.
      */
     public String iterativeF(int n) {
-        // TODO
-        return "";
+        if (n < 2) {
+            return String.valueOf(n);
+        }
+        BigInteger secLast = BigInteger.valueOf(0); // n-2
+        BigInteger last = BigInteger.valueOf(1); // n-1
+        BigInteger current = BigInteger.valueOf(0);
+        for (int i = 2; i < n; i++) {
+            current = secLast.add(last);
+            secLast = last;
+            last = current;
+        }
+        return String.valueOf(last);
+    }
+
+    public static void main(String[] args) {
+        AL01A al01A = new AL01A();
+        System.out.println(al01A.iterativeF(7));
+
     }
 }
