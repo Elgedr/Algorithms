a22f0a02029fe76bd535985df83b7e84e198ea56
elgedr <elgedr@ttu.ee>
2021-09-14 21:53:24 +0300

    commit

---
+++ AL02/src/ee/ttu/algoritmid/guessinggame/GuessingGame.java
@@ -0,0 +1,19 @@
+package ee.ttu.algoritmid.guessinggame;
+
+public class GuessingGame {
+
+    Oracle oracle;
+
+    public GuessingGame(Oracle oracle) {
+        this.oracle = oracle;
+    }
+
+    /**
+     * @param fruitArray - All the possible fruits.
+     * @return the name of the fruit.
+     */
+    public String play(Fruit[] fruitArray) {
+        // TODO
+        return "";
+    }
+}
